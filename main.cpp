a65c3a27db55bcfb32b56c4df94ecbb2d1b92346
grebrash <grebrash@ut.utm.edu>
2017-11-30 11:02:05 -0600

    Fourth commit: adding sum function to main.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int sum(int n);
+
 int main(){
+  int n;
+  
   cout << "Hello World!" << endl;
+  cout << "Enter number: ";
+  cin >> n;
+  cout << "The sum of 1 to n is: " << sum(n) << endl;
 
   return 0;
+}
+
+int sum(int n){
+  int total = 0;
+  for(int i = 1; i <= n; i++)
+    total += i;
+  return total;
 }
